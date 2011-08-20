2ed2d14ddf39da1fd746b2d8cdf6c9b60eebcc61
Billy Vong <billyvg@gmail.com>
2011-08-20 00:06:04 -0700

    Adding support to initialize Configuration table.

--- models/configuration.py
+++ models/configuration.py
@@ -22,30 +22,33 @@ class Configuration(Base, Model):
     name = Column(String(50), nullable=False)
     description = Column(String)
     value = Column(String, nullable=False)
-    
+
     def __init__(self, name=None, value=None, description=None, **kwargs):
         Model.__init__(self)
         Base.__init__(self)
     	self.name = name
     	self.value = value
     	self.description = description
-        
+
     def __repr__(self):
         """Pretty string formatting."""
-    
+
         return '<Configuration(%s, %s)>' % (self.name, self.value)
 	
 	def get(self, key):
 	    """Retrieves a configuration row by its key."""
 	
         return self.session.query(Configuration).filter_by(name=key).all()
-        
+
     def val(self, key):
         """Retrieves a configuration value by its key."""
-        
+
         return self.session.query(Configuration).filter_by(name=key).first().value
 
     def session_start(self):
+        pass
+
+    def initialize_table(self):
         # create the necessary tables in the database using the metadata
         # from the classes
         try:
@@ -54,6 +57,17 @@ class Configuration(Base, Model):
         except:
             print "Error: Could not connect to database."
             print traceback.print_exc()
+
+        self.session.add_all([
+            Configuration('network', 'localhost', 'The name of the network to connect to. (temp. )'),
+            Configuration('port', '6667', 'The port of the IRC server.'),
+            Configuration('nickname', 'ppbot', 'The nickname that the bot should use.'),
+            Configuration('password', '', 'The password for a server if necessary'),
+            Configuration('alt_nickname', 'ppbot_', 'An alternate nickname the bot should use if the primary is in use.'),
+            Configuration('realname', 'Powered by billy', 'The "real name" field displayed on /whois.'),
+            Configuration('me', 'billy', 'lol...'),
+            Configuration('trigger', '.', 'The trigger that the bot should respond to.'),
+        ])
 
         self.session.commit()
         self.session.close()

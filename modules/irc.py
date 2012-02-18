d946c205d1690c716dbc529e005f901b0cf12fab
Billy Vong <billyvg@gmail.com>
2012-02-17 23:51:49 -0800

    whitespace.

--- modules/irc.py
+++ modules/irc.py
@@ -4,9 +4,9 @@ class Irc(Module):
     """IRC Module to provide chat commands for common IRC actions."""
 
     def __init__(self, *args, **kwargs):
-	    """Constructor."""
-	    
-	    Module.__init__(self, kwargs=kwargs)
+        """Constructor."""
+
+        Module.__init__(self, kwargs=kwargs)
 
     def _register_events(self):
         """Register module events and commands."""
@@ -27,14 +27,14 @@ class Irc(Module):
         self.add_command('voice')
         self.add_command('debug')
         self.add_command('python')
-        
+
     def debug(self, event):
         print self.commands
         print self.modules
-        
+
     def python(self, event):
         self.msg(event['target'], 'hi')
-        
+
     @op
     def ban(self, event):
         """Bans a user from the channel.
@@ -47,18 +47,18 @@ class Irc(Module):
             self.server.mode(event['target'], '+b %s' % event['args'][0])
         else:
             self.syntax_message(event['nick'], '.ban <hostmask>')
- 
+
     @op
     def deop(self, event):
         """Deops a user from the specified channel or the current one."""
-        
+
         if self.num_args == 0:
             self.server.mode(event['target'], '-o %s' % event['nick'])
         elif self.num_args == 1:
             self.server.mode(event['target'], '-o %s' % event['args'][0])
         else:
             self.syntax_message(event['nick'], '.deop <nick>')
-    
+
     @op
     def devoice(self, event):
         """Devoices a user from the specified channel or the current one."""
@@ -69,28 +69,28 @@ class Irc(Module):
             self.server.mode(event['target'], '-v %s ' % event['args'][0])
         else:
             self.syntax_message(event['nick'], '.devoice <nick>')
-    
-    @op        	    
+
+    @op
     def invite(self, event):
         """Invites a user to a specified channel or the current one."""
-        
+
         if self.num_args == 1:
             self.server.invite(event['args'][0], event['target'])
         elif self.num_args == 2:
             self.server.invite(event['args'][0], event['args'][1])
         else:
             self.syntax_message(event['nick'], '.invite <nick> [channel]')
-    
+
     @master
     def join(self, event):
-		"""Makes the bot join a channel."""
+        """Makes the bot join a channel."""
 
-		if self.num_args == 1:
-			self.server.join(event['args'][0])
-		elif self.num_args == 2:
-			self.server.join(event['args'][0], event['args'][1])
-		else:
-			self.syntax_message(event['nick'], '.join <channel>')
+        if self.num_args == 1:
+            self.server.join(event['args'][0])
+        elif self.num_args == 2:
+            self.server.join(event['args'][0], event['args'][1])
+        else:
+            self.syntax_message(event['nick'], '.join <channel>')
 
     @op
     def kick(self, event):
@@ -102,11 +102,11 @@ class Irc(Module):
             self.server.kick(event['target'], event['args'][0], ' '.join(event['args'][1:]))
         else:
             self.syntax_message(event['nick'], '.kick <nick> [message]')
-    
+
     @master
     def msg(self, event):
-        """Messages a channel or user."""   
-        
+        """Messages a channel or user."""
+
         if self.num_args > 1:
             self.server.privmsg(event['args'][0], ' '.join(event['args'][1:]))
         elif self.num_args == 1:
@@ -122,35 +122,35 @@ class Irc(Module):
             self.server.nick(event['args'][0])
         else:
             self.syntax_message(event['nick'], '.nick <newnick>')
-    
+
     @op
     def giveop(self, event):
         """Ops a person in the specified channel, or the current channel."""
-        
+
         if self.num_args == 0:
             self.server.mode(event['target'], '+o %s' % event['nick'])
         elif self.num_args == 1:
             self.server.mode(event['target'], '+o %s' % event['args'][0])
         else:
             self.syntax_message(event['nick'], '.op <nick>')
-    
-    @master        
+
+    @master
     def part(self, event):
         """Makes the bot part a channel."""
-        
+
         if self.num_args == 1:
             self.server.part(event['args'][0])
         else:
             self.syntax_message(event['nick'], '.part <channel>')
-    
-    @master          
+
+    @master
     def quit(self, event):
         """Makes the bot quit IRC."""
-        
+
         message = "quit requested by %s" % event['nick']
         self.server.disconnect(message)
-    
-    @op 
+
+    @op
     def topic(self, event):
         """Changes the channel's topic."""
 
@@ -158,21 +158,21 @@ class Irc(Module):
             self.server.topic(event['target'], ' '.join(event['args'][0:]))
         else:
             self.syntax_message(event['nick'], '.topic <new topic>')
-    
-    @op   
+
+    @op
     def unban(self, event):
         """Unbans a user from the channel.
-	    
-	    TODO: Work on better userhost handling.
-	    TODO: Have this interact with the user database
-	    """
+
+        TODO: Work on better userhost handling.
+        TODO: Have this interact with the user database
+        """
 
         if self.num_args == 1:
             self.server.mode(event['target'], '-b %s' % event['args'][0])
         else:
             self.syntax_message(event['nick'], '.unban <hostmask>')
-     
-    @op       
+
+    @op
     def voice(self, event):
         """Voices a user from the specified channel or the current one."""
 

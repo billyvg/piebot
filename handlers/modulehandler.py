d24b32b8eda0bebf3f8e98b1b37d528606558502
Billy Vong <billyvg@gmail.com>
2012-03-05 17:48:40 -0800

    Whitespace.

--- handlers/modulehandler.py
+++ handlers/modulehandler.py
@@ -15,9 +15,9 @@ class ModuleHandler(Handler):
 
     def __init__(self, server, irc):
         """Constructor
-        
+
         @param the irclib server object
-        
+
         """
 
         self.server = server
@@ -28,12 +28,12 @@ class ModuleHandler(Handler):
 
     def load(self, name, class_name=None):
         """Loads a module.
-        
+
         @param name of the module
         @param <optional> name of the class
-        
+
         """
-        
+
         if not class_name:
             class_name = name
         # create a string in the form of "modules.<module name>.<class name>"
@@ -46,30 +46,30 @@ class ModuleHandler(Handler):
         except:
             print "Error importing module: %s" % name
             print traceback.print_exc()
-            
+
         try:
             for component_name in component_names[1:]:
                 mod = getattr(mod, component_name)
             module = mod(server=self.server, irc=self.irc)
             # keep a dictionary of modules, with the module name as the key
             self.modules[name] = module
-            
+
             print "Added module: %s" % name
-            
+
             # go through the module's commands list and keep a list of the commands
             # so that the message handler knows how to route the commnands
             for command, action in module.commands.iteritems():
-                # define a commands dict that has a key of the command name and 
+                # define a commands dict that has a key of the command name and
                 # value of the name of the x that holds the command
                 self.commands[command] = name
-                
+
             # also do something similar for events
             for event_type, action in module.events.iteritems():
-                try: 
+                try:
                     self.events[event_type][action] = name
                 except:
                     self.events[event_type] = {action: name}
-                
+
             return module
         except:
             print "Error instanciating class/module"
@@ -78,14 +78,14 @@ class ModuleHandler(Handler):
 
     def reload(self, module):
         """Reloads a module.
-        
+
         @param module name
-        
+
         """
-        
+
         # we can't reload the core module because we set the module_handler
         # when we first load it, and I can't think of a way to reset it
-        # maybe find a way to save the instance of the module_handler and 
+        # maybe find a way to save the instance of the module_handler and
         # then reload it.
         if module is not 'Core':
             try:

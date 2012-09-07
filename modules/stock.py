309cd21077e18432894f7df930314e47c4519727
Billy Vong <billyvg@gmail.com>
2012-09-07 13:56:33 -0700

    Add colors for stock module

--- modules/stock.py
+++ modules/stock.py
@@ -37,11 +37,21 @@ class Stock(Module):
             try:
                 stock_info = self.lookup_symbol(symbol)
                 # stylize the message output
-                message1 = "%(name)s (%(e)s:%(t)s) - %(l)s (%(c)s) - 52week high/low: (%(hi52)s/%(lo52)s)" % (stock_info)
+                change = float(stock_info['cp'])
+                if change > 0:
+                    color = "\x033"
+                elif change < 0:
+                    color = "\x034"
+                else:
+                    color = ""
+                stock_info['color'] = color
+                message1 = "%(name)s (%(e)s:%(t)s) - %(l)s (%(color)s%(c)s\x0f,%(color)s %(cp)s%%\x0f) - 52week high/low: (%(hi52)s/%(lo52)s) - MktCap: %(mc)s - P/E: %(pe)s" % (stock_info)
                 # send the messages
                 self.msg(event['target'], message1)
             except:
                 self.msg(event['target'], 'Could find symbol "%s"' % symbol)
+                import traceback
+                traceback.print_exc()
         else:
             self.syntax_message(event['nick'], '.stock <symbol>')
 

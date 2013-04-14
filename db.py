55d30777ce485f336d2dce47398a4960067207b5
Billy Vong <billyvg@gmail.com>
2013-04-14 01:34:07 -0700

    Remove ModuleMongo stuff

--- db.py
+++ db.py
@@ -5,33 +5,8 @@ Adds a wrapper to pymongo.
 
 """
 from pymongo import mongo_client
-from pymongo import database
-from pymongo import collection
 
 from settings import *
 
-class ModuleMongoClient(mongo_client.MongoClient):
-    def __getattr__(self, name):
-        attr = super(ModuleMongoClient, self).__getattr__(name)
-        if isinstance(attr, database.Database):
-            return ModuleDatabase(self, name)
-        return attr
-
-class ModuleDatabase(database.Database):
-    def __getattr__(self, name):
-        attr = super(ModuleDatabase, self).__getattr__(name)
-        if isinstance(attr, collection.Collection):
-            return ModuleCollection(self, name)
-        return attr
-
-class ModuleCollection(collection.Collection):
-    def __init__(self, database, name, create=False, **kwargs):
-        _name = 'module_%s_%s' % (self.__class__.__name__, name)
-        super(ModuleCollection, self).__init__(database=database,
-            name=_name,
-            create=create)
-
 client = mongo_client.MongoClient(MONGO_HOST, MONGO_PORT)
 db = client[MONGO_DB]
-module_client = ModuleMongoClient(MONGO_HOST, MONGO_PORT)
-module_db = module_client[MONGO_DB]

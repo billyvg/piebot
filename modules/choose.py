f374aadca4f97aaff157ac2f3bd47c4afb94ad77
Mezmor <lepswiss@gmail.com>
2013-11-04 00:12:24 -0800

    Added life councelor

---
+++ modules/choose.py
@@ -0,0 +1,29 @@
+"""
+
+If you're just lost in life and need something to make a choice for you.
+Ask our beloved dongbot!
+
+By Lucas/Mezmor
+
+@syntax .c <comma delimited list>
+
+"""
+
+from modules import *
+
+class Choose(Module):
+
+    def __init__(self, *args, **kwargs):
+        """Constructor"""
+        Module.__init__(self, kwargs=kwargs)
+        
+    def _register_events(self):
+        """Register module commands."""
+
+        self.add_command('c', 'choose')
+    
+    def choose(self, event):
+        # Make that bitch a one-liner
+        # Bitches love one-liners
+        # - Sentinel, Oct 28 2013
+        self.msg(event['target'], random.choice([ item.strip() for item in event['args'].split(",") if item ])

d870ff8bde590eeb5967886cce6372f2be845663
Billy Vong <billyvg@gmail.com>
2013-12-30 10:06:03 -0800

    Remove megaHAL implementation of chatbot

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -1,24 +1,82 @@
+"""
+Inspired from https://github.com/coleifer/irc/blob/master/bots/redisbot.py
+
+"""
+import re
+import random
+
 from modules import *
 
-import re
-
-try:
-    import mh_python
-except:
-    raise("You need the megaHAL for python installed.")
-
-
 class Chatbot(Module):
-    def __init__(self, server):
-        """Constructor."""
+    chain_length = 2
+    chattiness = 0.01
+    max_words = 20
+    messages_to_generate = 5
+    separator = '\x01'
+    stop_word = '\x02'
 
-        Module.__init__(self, server)
-        mh_python.initbrain()
-        
+    def __init__(self, *args, **kwargs):
+        """Constructor"""
+
+        Module.__init__(self, kwargs=kwargs)
+
     def _register_events(self):
         """Register module events and commands."""
 
         self.add_event('pubmsg', 'parse_message')
+
+    def make_key(self, k):
+        return k
+
+    def sanitize_message(self, message):
+        return re.sub('[\"\']', '', message.lower())
+
+    def split_message(self, message):
+        # split the incoming message into words, i.e. ['what', 'up', 'bro']
+        words = message.split()
+
+        # if the message is any shorter, it won't lead anywhere
+        if len(words) > self.chain_length:
+
+            # add some stop words onto the message
+            # ['what', 'up', 'bro', '\x02']
+            words.append(self.stop_word)
+
+            # len(words) == 4, so range(4-2) == range(2) == 0, 1, meaning
+            # we return the following slices: [0:3], [1:4]
+            # or ['what', 'up', 'bro'], ['up', 'bro', '\x02']
+            for i in range(len(words) - self.chain_length):
+                yield words[i:i + self.chain_length + 1]
+
+    def generate_message(self, seed):
+        key = seed
+
+        # keep a list of words we've seen
+        gen_words = []
+
+        # only follow the chain so far, up to <max words>
+        for i in xrange(self.max_words):
+
+            # split the key on the separator to extract the words -- the key
+            # might look like "this\x01is" and split out into ['this', 'is']
+            words = key.split(self.separator)
+
+            # add the word to the list of words in our generated message
+            gen_words.append(words[0])
+
+            # get a new word that lives at this key -- if none are present we've
+            # reached the end of the chain and can bail
+            next_word = self.db.markov.find_one({
+                'key': key
+            })
+
+            if not next_word or len(next_word.sequences) < 1:
+                break
+
+            # create a new key combining the end of the old one and the next_word
+            key = self.separator.join(words[1:] + [random.choose(next_word.sequences)])
+
+        return ' '.join(gen_words)
 
     def parse_message(self, event):
         """Parses any public messages to add it to the brain,
@@ -29,24 +87,39 @@ class Chatbot(Module):
 
         """
 
-        bot_nick = self.server.get_nickname()
         message = event['message']
-        
-        # check if the bot's name was used
-        m = re.search(bot_nick, message)
-        if m:
-            # we don't want the bot to learn its own nick
-            message = re.sub(bot_nick, '', message)
-            
-            # get the megaHAL reply
-            reply = "%s, %s" % (event['nick'], mh_python.doreply(message))
-            self.msg(event['target'], reply)
-            
-        self.learn_message(message)
-    
-    def learn_message(self, message):
-        """Passes the message to megahal brain to learn."""
-        
-        mh_python.learn(message)
-        mh_python.cleanup() 
+        messages = []
 
+        if self.pinged():
+            say_something = True
+            message = self.fix_ping(message)
+
+        # split up the incoming message into chunks that are 1 word longer than
+        # the size of the chain, e.g. ['what', 'up', 'bro'], ['up', 'bro', '\x02']
+        for words in self.split_message(self.sanitize_message(message)):
+            # grab everything but the last word
+            key = self.separator.join(words[:-1])
+
+            # add the last word to the set
+            self.db.markov.update({
+                'key': key
+            }, {
+                '$push': {
+                    'sequences': words[-1]
+                }
+            })
+
+                # if we should say something, generate some messages based on what
+            # was just said and select the longest, then add it to the list
+            if say_something:
+                best_message = ''
+                for i in range(self.messages_to_generate):
+                    generated = self.generate_message(seed=key)
+                    if len(generated) > len(best_message):
+                        best_message = generated
+
+                if best_message:
+                    messages.append(best_message)
+
+        if len(messages):
+            self.reply(random.choice(messages))
